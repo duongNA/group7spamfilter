61e583ea9388a26f437842fbdb7b5e2a9a9ac43e
letmsee <duong_ubuntu>
2013-11-16 17:50:53 +0700

    add two libraries: log4j for logging and junit for unit test

--- src/ict542/group7/spamfilter/test/TestMain.java
+++ src/ict542/group7/spamfilter/test/TestMain.java
@@ -1,8 +1,11 @@
 package ict542.group7.spamfilter.test;
 
+import org.apache.log4j.Logger;
+
 public class TestMain {
+	private static final Logger logger = Logger.getLogger(TestMain.class);
 	
 	public static final void main(String[] args) {
-		System.out.println("Hello world");
+		logger.debug("Hello world");
 	}
 }
---
+++ src/ict542/group7/spamfilter/unittest/FirstTest.java
@@ -0,0 +1,14 @@
+package ict542.group7.spamfilter.unittest;
+
+import org.junit.Test;
+
+import ict542.group7.spamfilter.test.TestMain;
+
+public class FirstTest {
+
+	@Test
+	public void testMain() {
+		TestMain.main(null);
+	}
+
+}
