ebb67c3cc3499bd36994a3d4605bfb358fe5b0b3
letmsee <duong_ubuntu>
2013-11-16 17:55:08 +0700

    fix project structure: cann't use test folder to put unit-test classes. Instead, we use src folder

--- src/ict542/group7/spamfilter/unittest/FirstTest.java
+++ src/ict542/group7/spamfilter/unittest/FirstTest.java
@@ -1,14 +1,5 @@
 package ict542.group7.spamfilter.unittest;
 
-import org.junit.Test;
-
-import ict542.group7.spamfilter.test.TestMain;
-
 public class FirstTest {
-
-	@Test
-	public void testMain() {
-		TestMain.main(null);
-	}
 
 }
